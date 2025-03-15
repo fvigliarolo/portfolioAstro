1644ba79f860e99d9009dc01a84789e147de1518
fvigliarolo <fd.vigliarolo@gmail.com>
2025-03-15 15:47:12 -0300

    Estructura inicial del portfolio desarrollado con astro

---
+++ src/data/projects.js
@@ -0,0 +1,68 @@
+const projects = [
+    {
+        title: "Agenda Barber System v2",
+        description: "This is the description of the project",
+        url: "",
+        tecnologies: ["Html", "CSS", "React", "MySQL", "Node.js"],
+        tasks: ["Development", "Infrastructure", "Deployment"],
+        tags: ["Web Development", "Responsive Web Development", "Backend Development", "Web Design", "Server Administration", "Optimization", "Refactoring", "Cloud Computing", "Backend Development", "Fullstack Development"]
+    },
+    {
+        title: "Box Assistant",
+        description: "This is the description of the project",
+        url: "",
+        tecnologies: ["Html", "CSS", "React", "Deploy Vercel", "Supabase", "NPM"],
+        tasks: ["Development", "Deployment"],
+        tags: ["Web Development", "Responsive Web Development", "Web Design", "Backend as a Service", "Infrastructure Administration"]
+    },
+    {
+        title: "Museums App",
+        description: "This is the description of the project",
+        url: "",
+        tecnologies: ["Html", "CSS", "JavaScript", "PHP", "MySQL", "GCP"],
+        tasks: ["R&D", "Teamwork", "Documentation", "Architecture Design"],
+        tags: ["Web Development", "Databases", "Cloud Computing"]
+    },
+    {
+        title: "Agenda Barber System v1",
+        description: "This is the description of the project",
+        url: "",
+        tecnologies: ["Html", "CSS", "JavaScript", "Bootstrap", "MySQL", "Node.js"],
+        tasks: ["Development"],
+        tags: ["Web Development", "Responsive Web Development", "Backend Development", "Fullstack Development", "Databases"]
+    },
+    {
+        title: "Administration Barber System",
+        description: "This is the description of the project",
+        url: "",
+        tecnologies: ["Html", "CSS", "React", "Deploy Hosting", "NPM"],
+        tasks: ["Development"],
+        tags: ["Web Development", "Responsive Web Development", "Web Design"]
+    },
+
+    {
+        title: "DBMS (EDA Project)",
+        description: "This is the description of the project",
+        url: "",
+        tecnologies: ["C Language"],
+        tasks: ["Development", "Teamwork", "Architecture Design"],
+        tags: ["Operating Systems", "Memory Management"]
+    },
+    {
+        title: "Hospital System (NoSQL Project)",
+        description: "This is the description of the project",
+        url: "",
+        tecnologies: ["Node.js", "TypeScript", "Firebase", "Postman", "Jenkins", "Docker", "NPM"],
+        tasks: ["R&D", "Teamwork", "Documentation", "Testing"],
+        tags: ["Backend Development", "Testing", "Automation Testing"]
+    },
+    {
+        title: "Gym App (PAP Project)",
+        description: "This is the description of the project",
+        url: "",
+        tecnologies: ["Java", "Maven"],
+        tasks: ["Backend Development", "Teamwork", "Documentation"],
+        tags: ["ORM", "Optimization"]
+    }
+];
+export default projects;
